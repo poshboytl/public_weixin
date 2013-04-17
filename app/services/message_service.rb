0ff4ee690f017876d14b561c10d62e2130315781
Terry Tai <poshboytl@gmail.com>
2013-04-18 01:40:42 +0800

    Order by hot.

--- app/services/message_service.rb
+++ app/services/message_service.rb
@@ -1,12 +1,18 @@
 class MessageService
-  attr_reader :request
+  attr_reader :request, :by_hot, :page
 
   def initialize(request, params)
     @request = request
     @page = (params[:page] || 1).to_i
+    @by_hot = !!params[:by_hot]
   end
 
   def index
-    Message.recent.page(@page).map { |message| MessagePresenter.new(request, message) }
+    if by_hot
+      messages = Message.by_vote.page(page)
+    else
+      messages = Message.recent.page(page)
+    end
+    messages.map { |message| MessagePresenter.new(request, message) }
   end
 end
