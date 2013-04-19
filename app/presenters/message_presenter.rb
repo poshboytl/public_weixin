e2efb17dc0610ddf05dca754445067b84aa323a5
Dingding Ye <yedingding@gmail.com>
2013-04-19 15:38:37 +0800

    url to full article

--- app/presenters/message_presenter.rb
+++ app/presenters/message_presenter.rb
@@ -7,7 +7,7 @@ class MessagePresenter
     @message = message
   end
 
-  delegate :title, :images, :content, :bizid, :msgid, to: :message
+  delegate :title, :images, :content, :bizid, :msgid, :url, to: :message
 
   def voted?
     service = VotingService.new(request, message.bizid, message.msgid)
